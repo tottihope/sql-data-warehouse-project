9877ebb44fd003923fbe38a7796c3c610d8296fd
tottihope <tottihope@gmail.com>
2026-05-04 21:01:41 +0100

    Create bronze layer CRM and ERP tables
    This script creates the bronze layer for CRM and ERP tables, including customer, product, sales details, and location tables.

---
+++ scripts/ddl_bronze.sql
@@ -0,0 +1,81 @@
+/*
+=====================================================================================
+DDL CRM & ERP tables 
+=====================================================================================
+*/
+
+-- create bronze layer CRM
+ -- cust_info
+IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info ;
+CREATE TABLE bronze.crm_cust_info (
+cst_id INT
+,cst_key NVARCHAR(50)
+,cst_firstname NVARCHAR(50)
+,cst_lastname NVARCHAR(50)
+,cst_marital_status NVARCHAR(50)
+,cst_gndr NVARCHAR(50)
+,cst_create_date DATE
+)
+GO
+
+-- prd
+ IF OBJECT_ID('bronze.crm_prd_info','U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info ;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INT 
+    , prd_key      VARCHAR(50)
+    , prd_nm       VARCHAR(50)
+    , prd_cost     INT
+    , prd_line     VARCHAR(50)
+    , prd_start_dt DATETIME
+    , prd_end_dt   DATETIME
+);
+GO
+
+-- sales_details
+ IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details ;
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num     VARCHAR(20)
+    , sls_prd_key   VARCHAR(50)
+    , sls_cust_id   INT
+    , sls_order_dt  INT
+    , sls_ship_dt   INT
+    , sls_due_dt    INT
+    , sls_sales     INT
+    , sls_quantity  INT
+    , sls_price     INT
+);
+GO
+
+-- create bronze layer ERP
+--  cust_az12
+IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12 ;
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50)
+    , bdate DATE
+    , gen   NVARCHAR(50)
+);
+GO
+
+-- loc_a101
+IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101 ;
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50)
+    , cntry NVARCHAR(50)
+);
+GO
+
+-- px_cat_g1v2
+ IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2 ;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50)
+    , cat        NVARCHAR(50)
+    , subcat     NVARCHAR(50)
+    , maintenace  NVARCHAR(50)
+);
+GO
