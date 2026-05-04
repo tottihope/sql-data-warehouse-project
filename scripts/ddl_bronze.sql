0aec33b06e928ec3c73e8ec9ff1f72595150a9f6
tottihope <tottihope@gmail.com>
2026-05-04 21:08:38 +0100

    Revise comments in ddl_bronze.sql
    Updated header comments for DDL script to reflect purpose and context.

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -1,7 +1,12 @@
 /*
-=====================================================================================
-DDL CRM & ERP tables 
-=====================================================================================
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
 */
 
 -- create bronze layer CRM
